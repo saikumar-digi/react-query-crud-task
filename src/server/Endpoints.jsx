ff556574876af6a130486898d581a15383e55695
saikumar <saikumar@digisprint.com>
2023-10-03 09:48:33 +0530

    used axios

--- src/server/Endpoints.jsx
+++ src/server/Endpoints.jsx
@@ -1,37 +1,34 @@
+import axios from "axios";
+
 export async function getAlltasks() {
-  const response = await fetch("http://localhost:3000/tasks");
-  return response.json();
+  const response = await axios.get("http://localhost:3000/tasks");
+  return response.data;
 }
 export async function createTask(newTask) {
-  const response = await fetch(`http://localhost:3000/tasks`, {
-    method: "POST",
+  const response = await axios.post(`http://localhost:3000/tasks`, newTask, {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(newTask),
   });
-  return response.json();
+  return response.data;
 }
 export async function updatedTask(updatedTask) {
-  const response = await fetch(
+  const response = await axios.put(
     `http://localhost:3000/Tasks/${updatedTask.id}`,
+    updatedTask,
     {
-      method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(updatedTask),
     }
   );
-  return response.json();
+  return response.data;
 }
 export async function getTaskById(id) {
-  const response = await fetch(`http://localhost:3000/tasks/${id}`);
-  return response.json();
+  const response = await axios.get(`http://localhost:3000/tasks/${id}`);
+  return response.data;
 }
 export async function deleteTask(id) {
-  const response = await fetch(`http://localhost:3000/tasks/${id}`, {
-    method: "DELETE",
-  });
-  return response.json();
+  const response = await axios.delete(`http://localhost:3000/tasks/${id}`);
+  return response.data;
 }
